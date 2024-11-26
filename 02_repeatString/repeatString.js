b7ab18483a4d425bd4fbdb10b3b192d00bb50b82
leoncol <camiloleon196@gmail.com>
2024-11-26 16:54:38 -0500

    Exercise 01 and 02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
 
-};
+const repeatString = function(string,num) {
+
+
+let newstring = '';
+    
+    
+for(let i = 0; i < num; i++){
+        newstring += string;
+       
+}       
+    if (num < 0){
+        return "ERROR";
+    } else {
+       return newstring;
+    }
+}
+;
+    
 
 // Do not edit below this line
 module.exports = repeatString;
